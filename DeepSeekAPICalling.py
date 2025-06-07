206b9c591c052065ad7f71e1bc68440eadd00798
lichunyan <1196298163@qq.com>
2025-06-07 10:26:28 +0800

    2.2.1 API参数详解-DeepSeek API简单调用示例

---
+++ DeepSeekAPICalling.py
@@ -0,0 +1,17 @@
+import requests
+import json
+api_key = 'sk-9052aa7aa92f48089b15fd204ffb0279'
+url = "https://api.deepseek.com/chat/completions"
+msg = [{"content":"You are a helpful assistant","role":"system"},{"content":"你好，今天星期几","role":"user"}]
+payload = json.dumps({
+  "messages": msg,
+  "model": "deepseek-chat",
+})
+headers = {
+  'Content-Type': 'application/json',
+  'Accept': 'application/json',
+  'Authorization': 'Bearer sk-9052aa7aa92f48089b15fd204ffb0279'
+}
+response = requests.request("POST", url, headers=headers, data=payload)
+print(type(response.text))
+print(response.text)
